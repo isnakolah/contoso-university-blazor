a0d4346c80bff75f4d09802e39b7014991afdee4
Daniel Nakolah <nakolahdaniel1@gmail.com>
2021-02-20 19:45:28 +0300

    Inject _mvcOptions and create a paginator

--- Pages/Students/Index.cshtml.cs
+++ Pages/Students/Index.cshtml.cs
@@ -3,23 +3,30 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ContosoUniversity.Models;
+using Microsoft.AspNetCore.Mvc;
+using ContosoUniversity.Data;
+using Microsoft.Extensions.Options;
+using System.Linq;
 
 namespace ContosoUniversity.Pages.Students
 {
     public class IndexModel : PageModel
     {
-        private readonly ContosoUniversity.Data.SchoolContext _context;
+        private readonly SchoolContext _context;
+        private readonly MvcOptions _mvcOptions;
 
-        public IndexModel(ContosoUniversity.Data.SchoolContext context)
+        public IndexModel(SchoolContext context, IOptions<MvcOptions> mvcOptions)
         {
             _context = context;
+            _mvcOptions = mvcOptions.Value;
         }
 
         public IList<Student> Student { get; set; }
 
         public async Task OnGetAsync()
         {
-            Student = await _context.Students.ToListAsync();
+            Student = await _context.Students.Take(
+             _mvcOptions.MaxModelBindingCollectionSize).ToListAsync();
         }
     }
 }
