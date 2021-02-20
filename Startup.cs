bd485e75975de98c6c78aa65f74676715c9c5089
Daniel Nakolah <nakolahdaniel1@gmail.com>
2021-02-20 19:43:37 +0300

    Add pagination and limiter

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,12 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using ContosoUniversity.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ContosoUniversity
 {
@@ -25,6 +22,13 @@ namespace ContosoUniversity
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var MyMaxModelBindingCollectionSize = 100;
+            Int32.TryParse(Configuration["MyMaxModelBindingCollectionSize"],
+                                        out MyMaxModelBindingCollectionSize);
+
+            services.Configure<MvcOptions>(options =>
+                    options.MaxModelBindingCollectionSize = MyMaxModelBindingCollectionSize);
+
             services.AddRazorPages();
 
             services.AddDbContext<SchoolContext>(options =>
